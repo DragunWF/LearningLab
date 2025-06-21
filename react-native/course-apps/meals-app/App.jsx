3d5faa53bb2c1a5cfa17a1be9da8b11563f0d270
DragunDev <plarisanmarc@gmail.com>
2025-06-21 23:05:41 +0800

    Update meals react native app

--- react-native/course-apps/meals-app/App.jsx
+++ react-native/course-apps/meals-app/App.jsx
@@ -2,12 +2,36 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailsScreen from "./screens/MealDetailsScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        sceneStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories",
+        }}
+      />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -22,22 +46,14 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
               title: "All Meal Categories",
+              headerShown: false,
             }}
           />
-          <Stack.Screen
-            name="MealsOverview"
-            component={MealsOverviewScreen}
-            // options={({ route, navigation }) => {
-            //   const categoryId = route.params.categoryId;
-            //   return {
-            //     title: categoryId,
-            //   };
-            // }}
-          />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
           <Stack.Screen
             name="MealDetails"
             component={MealDetailsScreen}
---
+++ react-native/course-apps/meals-app/screens/FavoritesScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from "react-native";
+
+function FavoritesScreen() {
+  return (
+    <View>
+      <Text>FavoritesScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default FavoritesScreen;
