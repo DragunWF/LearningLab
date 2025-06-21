d05f2a5e845373847ae7c7defb55e6155d9587cc
DragunDev <plarisanmarc@gmail.com>
2025-06-21 22:49:41 +0800

    Update navigator react native app

--- react-native/course-apps/navigator-app/App.jsx
+++ react-native/course-apps/navigator-app/App.jsx
@@ -1,15 +1,48 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
 
 const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
+
+function DrawerNavigation() {}
+
+function BottomTabNavigation() {}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
+      <BottomTab.Navigator
+        initialRouteName="User"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3c0a6b" },
+          headerTintColor: "white",
+          tabBarActiveTintColor: "#3c0a6b",
+        }}
+      >
+        <BottomTab.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <BottomTab.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
+      </BottomTab.Navigator>
+      {/* <Drawer.Navigator
         initialRouteName="User"
         screenOptions={{
           headerStyle: { backgroundColor: "#3c0a6b" },
@@ -38,7 +71,7 @@ export default function App() {
             ),
           }}
         />
-      </Drawer.Navigator>
+      </Drawer.Navigator> */}
     </NavigationContainer>
   );
 }
