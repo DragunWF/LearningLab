fd368eded9634f4067607b300b8b937f0849a758
DragunDev <plarisanmarc@gmail.com>
2025-06-21 22:52:14 +0800

    Update navigator react native app

--- react-native/course-apps/navigator-app/App.jsx
+++ react-native/course-apps/navigator-app/App.jsx
@@ -8,70 +8,79 @@ import UserScreen from "./screens/UserScreen";
 const Drawer = createDrawerNavigator();
 const BottomTab = createBottomTabNavigator();
 
-function DrawerNavigation() {}
-
-function BottomTabNavigation() {}
-
 export default function App() {
   return (
     <NavigationContainer>
-      <BottomTab.Navigator
-        initialRouteName="User"
-        screenOptions={{
-          headerStyle: { backgroundColor: "#3c0a6b" },
-          headerTintColor: "white",
-          tabBarActiveTintColor: "#3c0a6b",
-        }}
-      >
-        <BottomTab.Screen
-          name="Welcome"
-          component={WelcomeScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="home" color={color} size={size} />
-            ),
-          }}
-        />
-        <BottomTab.Screen
-          name="User"
-          component={UserScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="person" color={color} size={size} />
-            ),
-          }}
-        />
-      </BottomTab.Navigator>
-      {/* <Drawer.Navigator
-        initialRouteName="User"
-        screenOptions={{
-          headerStyle: { backgroundColor: "#3c0a6b" },
-          headerTintColor: "white",
-          drawerActiveBackgroundColor: "#f0e1ff",
-          drawerActiveTintColor: "#3c0a6b",
-          drawerStyle: { backgroundColor: "#ccc" },
-        }}
-      >
-        <Drawer.Screen
-          name="Welcome"
-          component={WelcomeScreen}
-          options={{
-            drawerLabel: "Welcome Screen",
-            drawerIcon: ({ color, size }) => (
-              <Ionicons name="home" color={color} />
-            ),
-          }}
-        />
-        <Drawer.Screen
-          name="User"
-          component={UserScreen}
-          options={{
-            drawerIcon: ({ color, size }) => (
-              <Ionicons name="person" color={color} size={size} />
-            ),
-          }}
-        />
-      </Drawer.Navigator> */}
+      {/* Comment one navigation component out and release the other for testing */}
+      <DrawerNavigation />
+      {/* <BottomTabNavigation /> */}
     </NavigationContainer>
+  );
+}
+
+function DrawerNavigation() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="User"
+      screenOptions={{
+        headerStyle: { backgroundColor: "#3c0a6b" },
+        headerTintColor: "white",
+        drawerActiveBackgroundColor: "#f0e1ff",
+        drawerActiveTintColor: "#3c0a6b",
+        drawerStyle: { backgroundColor: "#ccc" },
+      }}
+    >
+      <Drawer.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          drawerLabel: "Welcome Screen",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="User"
+        component={UserScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+function BottomTabNavigation() {
+  return (
+    <BottomTab.Navigator
+      initialRouteName="User"
+      screenOptions={{
+        headerStyle: { backgroundColor: "#3c0a6b" },
+        headerTintColor: "white",
+        tabBarActiveTintColor: "#3c0a6b",
+      }}
+    >
+      <BottomTab.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="User"
+        component={UserScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
   );
 }
