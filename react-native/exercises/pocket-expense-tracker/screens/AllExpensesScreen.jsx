204eeac7477dfcfc2181ce4cb3ae703d9260e6d9
DragunDev <plarisanmarc@gmail.com>
2025-06-22 21:07:11 +0800

    Update pocket-expense-tracker react native app

--- react-native/exercises/pocket-expense-tracker/screens/AllExpensesScreen.jsx
+++ react-native/exercises/pocket-expense-tracker/screens/AllExpensesScreen.jsx
@@ -2,12 +2,27 @@ import { StyleSheet, View, Text } from "react-native";
 
 function AllExpensesScreen() {
   return (
-    <View>
-      <Text>AllExpensesScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text>All Expenses</Text>
+      </View>
+      <View style={styles.expensesContainer}>
+        <Text>Expense List</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  headerContainer: {
+    marginTop: 20,
+  },
+  expensesContainer: {},
+});
 
 export default AllExpensesScreen;
