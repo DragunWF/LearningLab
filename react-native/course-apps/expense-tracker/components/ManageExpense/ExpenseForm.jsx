120af2ac44d84ae3b23c649175624a3ac4d928b1
DragunDev <plarisanmarc@gmail.com>
2025-06-26 19:26:04 +0800

    Update expense-tracker react native app

--- react-native/course-apps/expense-tracker/components/ManageExpense/ExpenseForm.jsx
+++ react-native/course-apps/expense-tracker/components/ManageExpense/ExpenseForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Alert } from "react-native";
 
 import Input from "./Input";
 import Button from "../ui/Button";
@@ -27,6 +27,15 @@ function ExpenseForm({ onCancel, onSubmit, submitButtonLabel, defaultValues }) {
       date: new Date(inputValues.date),
       description: inputValues.description,
     };
+
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== "Invalid Date";
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      Alert.alert("Invalid input", "Please check your input values!");
+      return;
+    }
 
     onSubmit(expenseData);
   }
