33261101338c6bc2342dd851f741174cf1dc24ee
DragunDev <plarisanmarc@gmail.com>
2025-06-26 12:19:05 +0800

    Update expense-tracker react native app

--- react-native/course-apps/expense-tracker/components/ExpensesOutput/ExpensesOutput.jsx
+++ react-native/course-apps/expense-tracker/components/ExpensesOutput/ExpensesOutput.jsx
@@ -2,6 +2,7 @@ import { StyleSheet, View } from "react-native";
 
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
 
 const DUMMY_EXPENSES = [
   {
@@ -38,13 +39,19 @@ const DUMMY_EXPENSES = [
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
 
 export default ExpensesOutput;
