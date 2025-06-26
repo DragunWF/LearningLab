9acd986e3ae000260a433ade7c0fb3d38e3062c6
DragunDev <plarisanmarc@gmail.com>
2025-06-26 14:26:37 +0800

    Update expense-tracker react native app

--- react-native/course-apps/expense-tracker/components/ExpensesOutput/ExpensesOutput.jsx
+++ react-native/course-apps/expense-tracker/components/ExpensesOutput/ExpensesOutput.jsx
@@ -4,39 +4,6 @@ import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2021-12-19"),
-  },
-  {
-    id: "e2",
-    description: "A pair of trousers",
-    amount: 59.99,
-    date: new Date("2022-01-05"),
-  },
-  {
-    id: "e3",
-    description: "Some mangoes",
-    amount: 5.99,
-    date: new Date("2021-12-01"),
-  },
-  {
-    id: "e4",
-    description: "Atomic Habits Book",
-    amount: 14.99,
-    date: new Date("2021-02-19"),
-  },
-  {
-    id: "e5",
-    description: "Psychology of Money Book",
-    amount: 18.59,
-    date: new Date("2021-02-22"),
-  },
-];
-
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
---
+++ react-native/course-apps/expense-tracker/store/expenses-context.jsx
@@ -0,0 +1,83 @@
+import { createContext, useReducer } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 59.99,
+    date: new Date("2022-01-05"),
+  },
+  {
+    id: "e3",
+    description: "Some mangoes",
+    amount: 5.99,
+    date: new Date("2021-12-01"),
+  },
+  {
+    id: "e4",
+    description: "Atomic Habits Book",
+    amount: 14.99,
+    date: new Date("2021-02-19"),
+  },
+  {
+    id: "e5",
+    description: "Psychology of Money Book",
+    amount: 18.59,
+    date: new Date("2021-02-22"),
+  },
+];
+
+// Added for auto-completion in the code editor
+export const ExpensesContext = createContext({
+  expenses: [],
+  addExpense: ({ description, amount, date }) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { description, amount, date }) => {},
+});
+
+function expensesReducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      const id = new Data().toString() + Math.random().toString();
+      return [{ ...action.payload, id: id }, ...state];
+    case "UPDATE":
+      const updatableExpenseIndex = state.findIndex(
+        (expense) => expense.id === action.payload.id
+      );
+      const updatableExpense = state[updatableExpenseIndex];
+      const updatedItem = { ...updatableExpense, ...action.payload.data };
+      const updatedExpenses = [...state];
+      updatedExpenses[updatableExpenseIndex] = updatedItem;
+      return updatedExpenses;
+    case "DELETE":
+      return state.filter((expense) => expense.id !== action.payload.id);
+    default:
+      return state;
+  }
+}
+
+function ExpensesContextProvider({ children }) {
+  const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
+
+  function addExpense(expenseData) {
+    dispatch({ type: "ADD", payload: expenseData });
+  }
+
+  function deleteExpense(id) {
+    dispatch({ type: "DELETE", payload: id });
+  }
+
+  function updateExpense(id) {
+    dispatch({ type: "UPDATE", payload: { id: id, data: expenseData } });
+  }
+
+  return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+}
+
+export default ExpensesContextProvider;
