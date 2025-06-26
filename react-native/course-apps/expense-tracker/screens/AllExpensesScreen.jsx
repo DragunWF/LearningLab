c14044d3faef529d20129aefdc680aeb2ad9e145
DragunDev <plarisanmarc@gmail.com>
2025-06-26 12:07:10 +0800

    Update expense-tracker react native app

--- react-native/course-apps/expense-tracker/screens/AllExpensesScreen.jsx
+++ react-native/course-apps/expense-tracker/screens/AllExpensesScreen.jsx
@@ -1,11 +1,8 @@
 import { StyleSheet, View, Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 function AllExpensesScreen() {
-  return (
-    <View>
-      <Text>AllExpensesScreen</Text>
-    </View>
-  );
+  return <ExpensesOutput expensesPeriod="Total" />;
 }
 
 const styles = StyleSheet.create({});
--- react-native/course-apps/expense-tracker/screens/RecentExpensesScreen.jsx
+++ react-native/course-apps/expense-tracker/screens/RecentExpensesScreen.jsx
@@ -1,11 +1,8 @@
 import { StyleSheet, View, Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 function RecentExpensesScreen() {
-  return (
-    <View>
-      <Text>RecentExpensesScreen</Text>
-    </View>
-  );
+  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
 }
 
 const styles = StyleSheet.create({});
