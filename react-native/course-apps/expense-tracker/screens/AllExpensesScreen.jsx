91ba40af81d1f605f57bc1c3e158834c261f95dd
DragunDev <plarisanmarc@gmail.com>
2025-06-26 11:32:23 +0800

    Update expense-tracker react native app

---
+++ react-native/course-apps/expense-tracker/screens/AllExpensesScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from "react-native";
+
+function AllExpensesScreen() {
+  return (
+    <View>
+      <Text>AllExpensesScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default AllExpensesScreen;
---
+++ react-native/course-apps/expense-tracker/screens/ManageExpensesScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from "react-native";
+
+function ManageExpensesScreen() {
+  return (
+    <View>
+      <Text>ManageExpensesScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ManageExpensesScreen;
