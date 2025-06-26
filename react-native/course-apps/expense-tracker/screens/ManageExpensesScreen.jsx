8fdd4ba9de08eb1b62d498caa1961e8c042c133f
DragunDev <plarisanmarc@gmail.com>
2025-06-26 13:06:29 +0800

    Update expense-tracker react native app

--- react-native/course-apps/expense-tracker/screens/ManageExpensesScreen.jsx
+++ react-native/course-apps/expense-tracker/screens/ManageExpensesScreen.jsx
@@ -1,6 +1,16 @@
+import { useLayoutEffect } from "react";
 import { StyleSheet, View, Text } from "react-native";
 
-function ManageExpensesScreen() {
+function ManageExpensesScreen({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return (
     <View>
       <Text>ManageExpensesScreen</Text>
