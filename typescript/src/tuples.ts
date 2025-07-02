ba9265d73e5a3edbc89d09ee6804c57cd83ce550
DragunDev <plarisanmarc@gmail.com>
2025-07-02 13:40:11 +0800

    Update tuples.ts

--- typescript/src/tuples.ts
+++ typescript/src/tuples.ts
@@ -1,3 +1,23 @@
-function tuplesMain() {}
+class TupleUser {
+  username: string;
+  password: string;
+
+  constructor(username: string, password: string) {
+    this.username = username;
+    this.password = password;
+  }
+}
+
+function tuplesMain() {
+  const position: [number, number] = [1, 2];
+  const values: [string, boolean, number, TupleUser] = [
+    "Hallo",
+    true,
+    23,
+    new TupleUser("admin", "password123"),
+  ];
+  console.log(position);
+  console.log(values);
+}
 
 tuplesMain();
