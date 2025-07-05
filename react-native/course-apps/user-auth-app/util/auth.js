db066d9c2b57f90d88b9997851f133fb5d5e36e4
DragunDev <plarisanmarc@gmail.com>
2025-07-05 16:37:26 +0800

    Update user auth react native app

---
+++ react-native/course-apps/user-auth-app/util/auth.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+// temporary key
+const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
+
+async function createUser(email, password) {
+  const response = await axios.post(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+    {
+      email,
+      password,
+      returnSecureToken: true,
+    }
+  );
+}
