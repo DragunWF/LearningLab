909c3ba3909807b92fdffb4e01fa24064de86438
DragunWF <plarisanmarc2@gmail.com>
2025-07-08 22:41:04 +0800

    Update favorite places react native app - Implement image picker for iOS

--- react-native/course-apps/favorite-places-app/components/Places/ImagePicker.jsx
+++ react-native/course-apps/favorite-places-app/components/Places/ImagePicker.jsx
@@ -1,8 +1,39 @@
-import { StyleSheet, View, Text, Button } from "react-native";
-import { launchCameraAsync } from "expo-image-picker";
+import { StyleSheet, View, Text, Button, Alert } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
 
 function ImagePicker() {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions for us to spy on you."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
   async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const chosenImage = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
