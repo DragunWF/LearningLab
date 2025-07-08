1e5d3ad88c0e8fc4326b6c00842567dfc3f72272
DragunWF <plarisanmarc2@gmail.com>
2025-07-08 22:26:27 +0800

    Update favorite places react native app - Implement image picker permissions

---
+++ react-native/course-apps/favorite-places-app/components/Places/ImagePicker.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from "react-native";
+
+function ImagePicker() {
+  return (
+    <View>
+      <Text>ImagePicker</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ImagePicker;
