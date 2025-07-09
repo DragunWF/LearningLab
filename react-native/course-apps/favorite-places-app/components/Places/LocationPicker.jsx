50f00aff76afea0a77ad708f121c0055114ff04f
DragunWF <plarisanmarc2@gmail.com>
2025-07-10 00:07:23 +0800

    Update favorite places react native app - add initial ui for location picker

---
+++ react-native/course-apps/favorite-places-app/components/Places/LocationPicker.jsx
@@ -0,0 +1,44 @@
+import { StyleSheet, View, Text } from "react-native";
+import Map from "../../screens/Map";
+import OutlinedButton from "../ui/OutlinedButton";
+import { Colors } from "../../constants/colors";
+import ImagePicker from "./ImagePicker";
+
+function LocationPicker() {
+  function getLocationHandler() {}
+
+  function pickOnMapHandler() {}
+
+  return (
+    <View>
+      <View style={styles.mapPreview}></View>
+      <View style={styles.actions}>
+        <OutlinedButton icon="location" onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon="map" onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default LocationPicker;
