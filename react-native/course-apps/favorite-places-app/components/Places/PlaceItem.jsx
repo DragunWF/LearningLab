b38505d39b916601e03d568a1a2a6d4f339c41fe
DragunWF <plarisanmarc2@gmail.com>
2025-07-10 02:20:10 +0800

    Update favorite places react native app - style place items and list

--- react-native/course-apps/favorite-places-app/components/Places/PlaceItem.jsx
+++ react-native/course-apps/favorite-places-app/components/Places/PlaceItem.jsx
@@ -1,17 +1,16 @@
 import { StyleSheet, View, Text, Pressable, Image } from "react-native";
+import { Colors } from "../../constants/colors";
 
 function PlaceItem({ place, onPress }) {
   return (
     <Pressable
       onPress={onPress}
-      style={({ pressed }) => pressed && styles.pressed}
+      style={({ pressed }) => [styles.item, pressed && styles.pressed]}
     >
-      <View>
-        <Image source={{ uri: place.imageUri }} />
-        <View>
-          <Text>{place.title}</Text>
-          <Text>{place.address}</Text>
-        </View>
+      <Image style={styles.image} source={{ uri: place.imageUri }} />
+      <View style={styles.info}>
+        <Text style={styles.title}>{place.title}</Text>
+        <Text style={styles.address}>{place.address}</Text>
       </View>
     </Pressable>
   );
@@ -19,8 +18,37 @@ function PlaceItem({ place, onPress }) {
 
 const styles = StyleSheet.create({
   pressed: {
-    opacity: 0.5,
+    opacity: 0.75,
   },
+  item: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    borderRadius: 6,
+    marginVertical: 12,
+    backgroundColor: Colors.primary500,
+    elevation: 2,
+    shadowColor: "black",
+    shadowOpacity: 0.15,
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 2,
+    borderRadius: 4,
+  },
+  image: {
+    flex: 1,
+    borderBottomLeftRadius: 4,
+    borderTopLeftRadius: 4,
+    height: 100,
+  },
+  info: {
+    flex: 2,
+    padding: 12,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: Colors.gray700,
+  },
+  address: {},
 });
 
 export default PlaceItem;
