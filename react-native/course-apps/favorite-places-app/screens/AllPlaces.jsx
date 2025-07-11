bbc94e35ac546bd6db70f1a06f9875df0207745a
DragunDev <plarisanmarc@gmail.com>
2025-07-11 20:23:52 +0800

    Update favorite places react native app - add fetching of places

--- react-native/course-apps/favorite-places-app/screens/AllPlaces.jsx
+++ react-native/course-apps/favorite-places-app/screens/AllPlaces.jsx
@@ -2,19 +2,22 @@ import { useEffect, useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { useIsFocused } from "@react-navigation/native";
 import PlacesList from "../components/Places/PlacesList";
+import { fetchPlaces } from "../util/database";
 
 function AllPlaces({ route }) {
   const [loadedPlaces, setLoadedPlaces] = useState([]);
   const isFocused = useIsFocused();
 
   useEffect(() => {
-    if (isFocused && route.params) {
-      setLoadedPlaces((currentPlaces) => [
-        ...currentPlaces,
-        route.params.place,
-      ]);
+    async function loadPlaces() {
+      const places = await fetchPlaces();
+      setLoadedPlaces(places);
     }
-  }, [isFocused, route.params]);
+
+    if (isFocused) {
+      loadPlaces();
+    }
+  }, [isFocused]);
 
   return <PlacesList places={loadedPlaces} />;
 }
