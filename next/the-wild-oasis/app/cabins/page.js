6a5a0524b542e5b815f9813909d5435e72083a10
DragunWF <plarisanmarc2@gmail.com>
2026-01-27 21:05:35 +0800

    feat(theWildOasis): update project

--- next/the-wild-oasis/app/cabins/page.js
+++ next/the-wild-oasis/app/cabins/page.js
@@ -1,7 +1,15 @@
-export default function Page() {
+export default async function Page() {
+  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await users.json();
+
   return (
     <div>
       <h1>Welcome to the Cabins Page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
