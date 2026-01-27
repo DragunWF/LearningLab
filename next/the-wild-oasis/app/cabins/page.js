949d46a6d3dac8775fff552d0b1652a18eda986b
DragunWF <plarisanmarc2@gmail.com>
2026-01-27 18:26:52 +0800

    feat(theWildOasis): update project

--- next/the-wild-oasis/app/cabins/page.js
+++ next/the-wild-oasis/app/cabins/page.js
@@ -1,9 +1,6 @@
-import Navigation from "../components/navigation";
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>Welcome to the Cabins Page</h1>
     </div>
   );
