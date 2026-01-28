9dc569a56c75916fe60ab8314c80def4b485ca94
DragunWF <plarisanmarc2@gmail.com>
2026-01-28 20:49:21 +0800

    feat(react): setup new project (the wild oasis original)

---
+++ react/course-apps/the-wild-oasis/src/App.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600px;
+`;
+
+function App() {
+  return <H1>Hello there, I am DragunWF</H1>;
+}
+
+export default App;
