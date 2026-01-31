2ef255f78d64f93f6c881c8c26a387d240240f5e
DragunWF <plarisanmarc2@gmail.com>
2026-01-31 13:54:40 +0800

    feat(react): setup supabase on the wild oasis

---
+++ react/course-apps/the-wild-oasis/src/services/apiCabins.js
@@ -0,0 +1,13 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+  
+  if (error) {
+    const errMessage = `Error fetching cabins: ${error.message}`;
+    console.error(errMessage);
+    throw new Error(errMessage);
+  }
+
+  return data;
+}
