21033c77c69f1d6ffc9dae267509488f526b78ee
DragunWF <plarisanmarc2@gmail.com>
2026-02-01 00:54:42 +0800

    feat(react): update the wild oasis and add hot toasts

--- react/course-apps/the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ react/course-apps/the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -50,10 +51,11 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
+      toast("Cabin deleted successfully");
       queryClient.invalidateQueries({ queryKey: ["cabins"] });
     },
     onError: (error) => {
-      alert(error.message);
+      toast(error.message);
       console.error(error.message);
     },
   });
