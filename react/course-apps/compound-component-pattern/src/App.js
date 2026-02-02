46d2f9cdb40106888d27f3032f5111fb292b7c10
DragunWF <plarisanmarc2@gmail.com>
2026-02-03 02:08:25 +0800

    feat(react): implement compound component pattern

--- react/course-apps/compound-component-pattern/src/App.js
+++ react/course-apps/compound-component-pattern/src/App.js
@@ -5,14 +5,34 @@ export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      <Counter
+      {/* <Counter
         iconIncrease="+"
         iconDecrease="-"
         label="My NOT so flexible counter"
         hideLabel={false}
         hideIncrease={false}
         hideDecrease={false}
-      />
+      /> */}
+
+      <div>
+        <Counter>
+          <Counter.Label>My super flexible counter by DragunWF</Counter.Label>
+          <Counter.Increase icon="+" />
+          <Counter.Count />
+          <Counter.Decrease icon="-" />
+        </Counter>
+      </div>
+
+      <div>
+        <Counter>
+          <Counter.Increase icon="+" />
+          <div>
+            <Counter.Count />
+          </div>
+          <Counter.Decrease icon="-" />
+          <Counter.Label>My super flexible counter by DragunWF</Counter.Label>
+        </Counter>
+      </div>
     </div>
   );
 }
