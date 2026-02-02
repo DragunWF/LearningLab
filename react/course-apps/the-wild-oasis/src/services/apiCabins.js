e2e5243ca294e396f3513c201199a00eb2941065
DragunWF <plarisanmarc2@gmail.com>
2026-02-02 15:18:54 +0800

    feat(react): update the wild oasis [implement cabin creation]

--- react/course-apps/the-wild-oasis/src/services/apiCabins.js
+++ react/course-apps/the-wild-oasis/src/services/apiCabins.js
@@ -12,6 +12,21 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    const errMessage = `Error creating cabin: ${error.message}`;
+    console.error(errMessage);
+    throw new Error(errMessage);
+  }
+
+  return data;
+}
+
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
