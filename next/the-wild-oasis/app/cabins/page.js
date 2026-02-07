846abda1282416e74ce60ec44b6e7954b8ff3791
DragunWF <plarisanmarc2@gmail.com>
2026-02-08 00:20:51 +0800

    feat(next): the wild oasis [project organization]

--- next/the-wild-oasis/app/cabins/page.js
+++ next/the-wild-oasis/app/cabins/page.js
@@ -1,19 +1,7 @@
-import Counter from "../components/Counter";
-
 export default async function Page() {
-  const users = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await users.json();
-
   return (
     <div>
-      <h1>Welcome to the Cabins Page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-
-      <Counter userCount={data.length} />
+      <h1>Cabins Page</h1>
     </div>
   );
 }
