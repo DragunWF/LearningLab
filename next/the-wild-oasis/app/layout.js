9065f93f04e8ba8fed7e44208ac7c918b221c315
DragunWF <plarisanmarc2@gmail.com>
2026-02-08 22:29:58 +0800

    feat(next): import josefin font to the wild oasis project

--- next/the-wild-oasis/app/layout.js
+++ next/the-wild-oasis/app/layout.js
@@ -1,6 +1,14 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/navigation";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap", // first displays default font then swaps to Josefin font
+  variable: "--font-josefin-sans",
+});
+
 export const metadata = {
   title: {
     template: "%s | The Wild Oasis",
@@ -13,7 +21,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-gray-50 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-gray-50 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
