2ef593b33c5402105fa6fb17717def2d220e0c5c
DragunWF <plarisanmarc2@gmail.com>
2026-02-12 00:21:26 +0800

    feat(next): implement server actions for updating guest profile

--- next/the-wild-oasis/app/_lib/data-service.js
+++ next/the-wild-oasis/app/_lib/data-service.js
@@ -196,6 +196,7 @@ export async function createBooking(newBooking) {
 // UPDATE
 
 // The updatedFields is an object which should ONLY contain the updated data
+/*
 export async function updateGuest(id, updatedFields) {
   const { data, error } = await supabase
     .from("guests")
@@ -238,3 +239,4 @@ export async function deleteBooking(id) {
   }
   return data;
 }
+*/
